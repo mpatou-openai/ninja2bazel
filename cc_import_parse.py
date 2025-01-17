b9bcc205096191899284bbccd38d19cb8aee6a54
Matthieu Patou <mat@matws.net>
2025-01-17 14:53:35 -0800

    Change parsing of cc_import to deal with multi line properly

--- cc_import_parse.py
+++ cc_import_parse.py
@@ -5,6 +5,43 @@ from typing import Optional
 
 from bazel import BazelCCImport
 
+def _processValue(inflightVals: str, inflightAttr: str, current: BazelCCImport):
+    inGlob = False
+    assert inflightVals is not None
+    assert inflightAttr is not None
+    assert current is not None
+    tmp = []
+    vals = []
+    for v in inflightVals.split("\n"):
+        val = v.strip()
+        if val.startswith("glob(["):
+            if match_glob(val):
+                vals.extend(parse_glob(val))
+            else:
+                inGlob = True
+                tmp.append(val)
+        elif inGlob:
+            tmp.append(val)
+        else:
+            if val.endswith(","):
+                val = val[:-1]
+            for subVal in val.split(","):
+                if len(subVal) == 0:
+                    continue
+                if subVal[0] == "[":
+                    subVal = subVal[1:]
+                if val[-1] == "]":
+                    subVal = subVal[:-1]
+                subVal = subVal.strip()
+                # replace quotes ...
+                subVal =subVal.replace('"', "").replace("'", "")
+                if len(subVal) > 0:
+                    vals.append(subVal)
+    if inGlob:
+        inGlob = False
+        vals.extend(parse_glob("".join(tmp)))
+        tmp = []
+    setattr(current, inflightAttr, vals)
 
 def parseCCImports(raw_imports: list[str], location: str) -> list[BazelCCImport]:
     imports = []
@@ -13,7 +50,6 @@ def parseCCImports(raw_imports: list[str], location: str) -> list[BazelCCImport]
     name = ""
     inflightAttr = None
     inflightVals = None
-    inflightComplete = False
     for line in raw_imports:
 
         line = line.strip()
@@ -33,118 +69,49 @@ def parseCCImports(raw_imports: list[str], location: str) -> list[BazelCCImport]
                 )
             newObj = False
             assert current is not None
+            if inflightVals is not None:
+                _processValue(inflightVals, inflightAttr, current)
+                inflightVals = None
             imports.append(current)
             current = None
             continue
+                
 
-        if "=" in line:
-            val = line.split("=")[1].strip()
+        regexAttribute = r"(.*)\s*=\s*(.*)"
+        r = re.match(regexAttribute, line)
+        if r:
+            attribute = r.group(1).strip()
+            val = r.group(2).strip()
+            if inflightVals is not None:
+                _processValue(inflightVals, inflightAttr, current)
+                inflightVals = None
 
-            if line.startswith("name = "):
+            if attribute == "name":
                 name = cleanupVar(val)
                 current = BazelCCImport(name)
                 # Force this external repo
                 current.setLocation("@cpp_ext_libs//")
                 current.setPhysicalLocation(location)
-            if line.startswith("interface_library = ") or line.startswith(
-                "shared_library = "
-            ):
+            if attribute in ["interface_library", "shared_library"]:
                 assert current is not None
                 current.setSharedLibrarys(cleanupVar(val))
-            if line.startswith("skip_wrapping = "):
+            if attribute == "skip_wrapping":
                 assert current is not None
                 current.setSkipWrapping(val == "True")
-            if line.startswith("static_library = "):
+            if attribute == "static_library":
                 assert current is not None
                 current.setStaticLibrarys(cleanupVar(val))
-            if line.startswith("static_libs = "):
+            if attribute == "static_libs":
                 assert current is not None
                 current.setStaticLibrarys(cleanupVar(val))
-            if line.startswith("deps = "):
+            if attribute in ["deps", "hdrs", "includes"]:
                 assert current is not None
-                regex = r"([()\[\]])"
-                openParentesis = 0
-                openBrackets = 0
-                found = re.findall(regex, line)
-                for c in found:
-                    if c == "[":
-                        openBrackets += 1
-                    if c == "]":
-                        openBrackets -= 1
-                    if c == "(":
-                        openParentesis += 1
-                    if c == ")":
-                        openParentesis -= 1
-                inflightAttr = "deps"
-                inflightVals = line.replace("deps = ", "").strip()
-                inflightComplete = False
-                if openParentesis == 0 or openBrackets == 0:
-                    inflightComplete = True
-            if line.startswith("includes = "):
-                assert current is not None
-                regex = r"([\[\]])"
-                openBrackets = 0
-                found = re.findall(regex, line)
-                for c in found:
-                    if c == "[":
-                        openBrackets += 1
-                    if c == "]":
-                        openBrackets -= 1
-                inflightAttr = "includes"
-                inflightVals = line.replace("includes = ", "").strip()
-                inflightComplete = False
-                if openBrackets == 0:
-                    inflightComplete = True
-            if line.startswith("hdrs = "):
-                assert current is not None
-                regex = r"([()\[\]])"
-                openParentesis = 0
-                openBrackets = 0
-                found = re.findall(regex, line)
-                for c in found:
-                    if c == "[":
-                        openBrackets += 1
-                    if c == "]":
-                        openBrackets -= 1
-                    if c == "(":
-                        openParentesis += 1
-                    if c == ")":
-                        openParentesis -= 1
-                inflightAttr = "hdrs"
-                inflightVals = line.replace("hdrs = ", "").strip()
-                inflightComplete = False
-                if openParentesis == 0 or openBrackets == 0:
-                    inflightComplete = True
-
+                inflightAttr = attribute
+                inflightVals = val
         else:
-            logging.info(line)
-        if inflightComplete:
-            assert inflightVals is not None
-            assert inflightAttr is not None
-            assert current is not None
-            inflightComplete = False
-            vals = []
-            for v in inflightVals.split("\n"):
-                val = v.strip()
-                if val.endswith(","):
-                    val = val[:-1]
-                if val.startswith("glob("):
-                    vals.extend(parse_glob(val))
-                else:
-                    for subVal in val.split(","):
-                        if len(subVal) == 0:
-                            continue
-                        if subVal[0] == "[":
-                            subVal = subVal[1:]
-                        if val[-1] == "]":
-                            subVal = subVal[:-1]
-                        subVal = subVal.strip()
-                        # replace quotes ...
-                        subVal =subVal.replace('"', "").replace("'", "")
-                        if len(subVal) > 0 and subVal[0] == ":":
-                            subVal = subVal[1:]
-                        vals.append(subVal)
-            setattr(current, inflightAttr, vals)
+            if inflightVals is not None:
+                inflightVals += f"\n{line}"
+
 
     return imports
 
@@ -153,20 +120,27 @@ def cleanupVar(var: str) -> str:
     return var.replace('"', "").replace("'", "").replace(",", "").strip()
 
 
+def match_glob(raw_glob: str) -> bool:
+    regex = r'glob\(\s*\[(.*)\]\s*\)'
+    matches = re.search(regex, raw_glob)
+    return matches is not None
+
 def parse_glob(raw_glob: str) -> list[str]:
     ret: list[str] = []
+    regex = r'glob\(\s*\[(.*)\]\s*\)'
 
-    for e in raw_glob[:-1].replace("glob(", "").split(","):
+    logging.debug(f"Processing glob: {raw_glob}")
+    matches = re.search(regex, raw_glob)
+    if not matches or not matches.group(1):
+        logging.error(f"Error parsing glob: {raw_glob}")
+        raise ValueError(f"Error parsing not matching this regex {regex}")
+
+    for e in matches.group(1).split(","):
         e = e.strip()
-        if e[0] == "[":
-            e = e[1:]
-        if e[-1] == "]":
-            e = e[:-1]
         pattern = e.replace('"', "").replace("'", "")
         matching_files = glob.glob(pattern, recursive=True)
 
         # Print the matching files
         for file in matching_files:
             ret.append(file)
-
     return ret
